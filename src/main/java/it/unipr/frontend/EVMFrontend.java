6ef68c09108df86495faa47fd12207de464564a3
Utente <Utente@DESKTOP-MFURO9K>
2022-06-15 16:16:54 +0200

    Method for getting EVM bytecode from Sm address

--- src/main/java/it/unipr/frontend/EVMFrontend.java
+++ src/main/java/it/unipr/frontend/EVMFrontend.java
@@ -8,14 +8,18 @@ import java.nio.charset.StandardCharsets;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 //import org.antlr.v4.runtime.tree.ParseTree;
-
-import it.unipr.evm.antlr.EVMBLexer;
 import it.unipr.evm.antlr.EVMBParser;
+import it.unipr.evm.antlr.EVMBLexer;
 
 public class EVMFrontend {
 	
 	public static void main(String[] args) throws IOException {
+		String result = SMAddress.parseSMAddress("0x6f5bB7cC4F3D6628d0095545552757AB377FE15C");
+		@SuppressWarnings("unused")
+		BytecodeToOpcode trad = new BytecodeToOpcode("evm-testcases/sm.sol");
+		BytecodeToOpcode.translate(result);
 		parseContract("evm-testcases/sm.sol");
+		System.out.println(result);
 	}
 	
 	public static void parseContract(String filePath) throws IOException {
