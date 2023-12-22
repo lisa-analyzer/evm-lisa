7d629e5d146a80d2a8a15a9be6af182465ad323a
VincenzoArceri <vincenzoarceri.92@gmail.com>
2023-12-22 16:01:39 +0100

    Refactoring cfs tests

--- src/test/java/it/unipr/analysis/cron/EVMBytecodeControlFlowStructureTest.java
+++ src/test/java/it/unipr/analysis/cron/EVMBytecodeControlFlowStructureTest.java
@@ -60,46 +60,46 @@ public class EVMBytecodeControlFlowStructureTest extends EVMBytecodeAnalysisExec
 		return conf;
 	}
 
-//	@Test
-//	public void testIf() throws AnalysisSetupException, IOException {
-//		CronConfiguration conf = createConfiguration("cfs/if", "if_eth.sol", false);
-//		perform(conf);
-//	}
-//
-//	@Test
-//	public void testIfElse() throws AnalysisSetupException, IOException {
-//		CronConfiguration conf = createConfiguration("cfs/if_else", "if_else_eth.sol", false);
-//		perform(conf);
-//	}
-//
-//	@Test
-//	public void testWhile() throws AnalysisSetupException, IOException {
-//		CronConfiguration conf = createConfiguration("cfs/while", "while_eth.sol", false);
-//		perform(conf);
-//	}
-//
-//	@Test
-//	public void testIfElseNPBJ() throws AnalysisSetupException, IOException {
-//		CronConfiguration conf = createConfiguration("cfs/if_else_npbj", "if_else_npbj_eth.sol", false);
-//		perform(conf);
-//	}
-//
-//	@Test
-//	public void testWhileNPBJ() throws AnalysisSetupException, IOException {
-//		CronConfiguration conf = createConfiguration("cfs/while_npbj", "while_npbj_eth.sol", false);
-//		perform(conf);
-//	}
-//
-//	/**
-//	 * Testcase for a real contract: 0x0000000000bda2152794ac8c76b2dc86cba57cad
-//	 * - Number of opcodes: 32
-//	 * - Expected solved jumps %: 100%
-//	 */
-//	@Test
-//	public void testRealContract() throws AnalysisSetupException, IOException {
-//		CronConfiguration conf = createConfiguration("cfs/0x0000000000bda2152794ac8c76b2dc86cba57cad", "0x0000000000bda2152794ac8c76b2dc86cba57cad.sol", false);
-//		perform(conf);
-//	}
+	@Test
+	public void testIf() throws AnalysisSetupException, IOException {
+		CronConfiguration conf = createConfiguration("cfs/if", "if_eth.sol", false);
+		perform(conf);
+	}
+
+	@Test
+	public void testIfElse() throws AnalysisSetupException, IOException {
+		CronConfiguration conf = createConfiguration("cfs/if_else", "if_else_eth.sol", false);
+		perform(conf);
+	}
+
+	@Test
+	public void testWhile() throws AnalysisSetupException, IOException {
+		CronConfiguration conf = createConfiguration("cfs/while", "while_eth.sol", false);
+		perform(conf);
+	}
+
+	@Test
+	public void testIfElseNPBJ() throws AnalysisSetupException, IOException {
+		CronConfiguration conf = createConfiguration("cfs/if_else_npbj", "if_else_npbj_eth.sol", false);
+		perform(conf);
+	}
+
+	@Test
+	public void testWhileNPBJ() throws AnalysisSetupException, IOException {
+		CronConfiguration conf = createConfiguration("cfs/while_npbj", "while_npbj_eth.sol", false);
+		perform(conf);
+	}
+
+	/**
+	 * Testcase for a real contract: 0x0000000000bda2152794ac8c76b2dc86cba57cad
+	 * - Number of opcodes: 32
+	 * - Expected solved jumps %: 100%
+	 */
+	@Test
+	public void testRealContract() throws AnalysisSetupException, IOException {
+		CronConfiguration conf = createConfiguration("cfs/0x0000000000bda2152794ac8c76b2dc86cba57cad", "0x0000000000bda2152794ac8c76b2dc86cba57cad.sol", false);
+		perform(conf);
+	}
 //	
 //	@Test
 //	public void testMemory() throws AnalysisSetupException, IOException {
