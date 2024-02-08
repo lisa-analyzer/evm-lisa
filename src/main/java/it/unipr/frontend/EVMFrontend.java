90fbc9f3bf131b94e674aca51993a435ded5e2bb
VincenzoArceri <vincenzoarceri.92@gmail.com>
2024-02-08 16:45:05 +0100

    Fixed opcode generation

--- src/main/java/it/unipr/frontend/EVMFrontend.java
+++ src/main/java/it/unipr/frontend/EVMFrontend.java
@@ -135,10 +135,7 @@ public class EVMFrontend {
 			}
 
 			else {
-				if (addOpcode(opcode, writer)) {
-					writer.close();
-					return true;
-				}
+				addOpcode(opcode, writer);
 			}
 		}
 
@@ -209,7 +206,7 @@ public class EVMFrontend {
 	 * 
 	 * @throws IOException
 	 */
-	private static boolean addOpcode(String opcode, Writer writer) throws IOException {
+	private static void addOpcode(String opcode, Writer writer) throws IOException {
 		switch (opcode) {
 		case "00":
 			writer.write("STOP\n");
@@ -547,16 +544,14 @@ public class EVMFrontend {
 			break;
 		case "fe":
 			writer.write("INVALID\n");
-			return true;
+			break;
 		case "ff":
 			writer.write("SELFDESTRUCT\n");
 			break;
 		default:
 			writer.write("'" + opcode + "'" + "(Unknown Opcode)\n");
 			break;
-		}
-		
-		return false;
+		}		
 	}
 
 	private static int pushTest(String opcode) {
