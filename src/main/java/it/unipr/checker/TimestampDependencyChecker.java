bac5d57c9682bd2f77d7ac9c26e123310d38066f
VincenzoArceri <vincenzoarceri.92@gmail.com>
2024-12-16 15:51:38 +0100

    Timestamp dependency checker 's draft

---
+++ src/main/java/it/unipr/checker/TimestampDependencyChecker.java
@@ -0,0 +1,39 @@
+package it.unipr.checker;
+
+import it.unipr.analysis.taint.TaintAbstractStack;
+import it.unive.lisa.analysis.SimpleAbstractState;
+import it.unive.lisa.analysis.heap.MonolithicHeap;
+import it.unive.lisa.analysis.nonrelational.value.TypeEnvironment;
+import it.unive.lisa.analysis.types.InferredTypes;
+import it.unive.lisa.checks.semantic.CheckToolWithAnalysisResults;
+import it.unive.lisa.checks.semantic.SemanticCheck;
+import it.unive.lisa.program.cfg.CFG;
+import it.unive.lisa.program.cfg.statement.Statement;
+
+public class TimestampDependencyChecker implements
+SemanticCheck<SimpleAbstractState<MonolithicHeap, TaintAbstractStack, TypeEnvironment<InferredTypes>>> {
+
+
+	@Override
+	public void beforeExecution(
+			CheckToolWithAnalysisResults<SimpleAbstractState<MonolithicHeap, TaintAbstractStack, TypeEnvironment<InferredTypes>>> tool) {
+		// TODO Auto-generated method stub
+		SemanticCheck.super.beforeExecution(tool);
+	}
+
+	@Override
+	public boolean visit(
+			CheckToolWithAnalysisResults<SimpleAbstractState<MonolithicHeap, TaintAbstractStack, TypeEnvironment<InferredTypes>>> tool,
+			CFG graph, Statement node) {
+		// TODO Auto-generated method stub
+		return SemanticCheck.super.visit(tool, graph, node);
+	}
+
+	@Override
+	public void afterExecution(
+			CheckToolWithAnalysisResults<SimpleAbstractState<MonolithicHeap, TaintAbstractStack, TypeEnvironment<InferredTypes>>> tool) {
+		// TODO Auto-generated method stub
+		SemanticCheck.super.afterExecution(tool);
+	}
+
+}
