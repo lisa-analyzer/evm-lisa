d6e18b21be99edced32c4d1de634e9007252a394
0xMere <merenda.s.mattia@gmail.com>
2025-01-09 17:32:39 +0100

    fix: added solidifi's tx-origin bytecodes

--- src/test/java/it/unipr/analysis/cron/EVMBytecodeSolidiFITxOriginTruth.java
+++ src/test/java/it/unipr/analysis/cron/EVMBytecodeSolidiFITxOriginTruth.java
@@ -3,6 +3,7 @@ package it.unipr.analysis.cron;
 import it.unipr.analysis.EVMAbstractState;
 import it.unipr.analysis.MyCache;
 import it.unipr.analysis.taint.TaintAbstractDomain;
+import it.unipr.analysis.taint.TxOriginAbstractDomain;
 import it.unipr.checker.JumpSolver;
 import it.unipr.checker.TxOriginChecker;
 import it.unipr.frontend.EVMFrontend;
@@ -77,7 +78,7 @@ public class EVMBytecodeSolidiFITxOriginTruth {
 					lisa.run(program);
 
 					conf.abstractState = new SimpleAbstractState<>(
-							new MonolithicHeap(), new TaintAbstractDomain(),
+							new MonolithicHeap(), new TxOriginAbstractDomain(),
 							new TypeEnvironment<>(new InferredTypes()));
 
 					conf.semanticChecks.clear();
