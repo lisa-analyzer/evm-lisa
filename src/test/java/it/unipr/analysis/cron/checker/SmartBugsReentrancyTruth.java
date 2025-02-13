d4019e480f0ff9ad73bb0650e223336eff0a572e
VincenzoArceri <vincenzoarceri.92@gmail.com>
2025-02-13 11:06:54 +0100

    Renaming some tests files

--- src/test/java/it/unipr/analysis/cron/checker/SmartBugsReentrancyTruth.java
+++ src/test/java/it/unipr/analysis/cron/checker/SmartBugsReentrancyTruth.java
@@ -24,8 +24,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Ignore;
 
-public class EVMBytecodeSmartBugsReentrancyTruth {
-	private static final Logger log = LogManager.getLogger(EVMBytecodeSmartBugsReentrancyTruth.class);
+public class SmartBugsReentrancyTruth {
+	private static final Logger log = LogManager.getLogger(SmartBugsReentrancyTruth.class);
 
 	private ConcurrentMap<Integer, Integer> _results = new ConcurrentHashMap<>();
 
