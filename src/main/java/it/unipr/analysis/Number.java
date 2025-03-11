04af8af78bbc85a4f2b6044dc40644cac50e9b5d
VincenzoArceri <vincenzoarceri.92@gmail.com>
2025-03-11 16:22:42 +0100

    Fixing add semantics in Number

--- src/main/java/it/unipr/analysis/Number.java
+++ src/main/java/it/unipr/analysis/Number.java
@@ -133,7 +133,7 @@ public class Number implements Comparable<Number> {
 		if (this.getType() == other.getType() && other.getType() == Type.INT)
 			return new Number(i + other.getInt());
 		if (this.getType() == other.getType() && other.getType() == Type.LONG)
-			return new Number((long) l - other.getLong());
+			return new Number((long) l + other.getLong());
 
 		BigInteger me = toBigInteger(this);
 		BigInteger ot = toBigInteger(other);
