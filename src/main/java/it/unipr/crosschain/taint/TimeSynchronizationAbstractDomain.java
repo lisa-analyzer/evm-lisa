88dac9366a77c066f1bdd78251de4f4a39850aa5
0xMere <merenda.s.mattia@gmail.com>
2025-03-17 15:13:21 +0100

    Added TimeSynchronizationAbstractDomain for cross-chain taint analysis

---
+++ src/main/java/it/unipr/crosschain/taint/TimeSynchronizationAbstractDomain.java
@@ -0,0 +1,71 @@
+package it.unipr.crosschain.taint;
+
+import it.unipr.analysis.taint.TaintAbstractDomain;
+import it.unipr.analysis.taint.TaintElement;
+import it.unipr.cfg.*;
+import it.unipr.cfg.Number;
+import it.unive.lisa.program.cfg.statement.Statement;
+import it.unive.lisa.symbolic.value.Operator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Set;
+
+public class TimeSynchronizationAbstractDomain extends TaintAbstractDomain {
+
+	private static final TimeSynchronizationAbstractDomain TOP = new TimeSynchronizationAbstractDomain(
+			new ArrayList<>(Collections.nCopies(TaintAbstractDomain.STACK_LIMIT, TaintElement.BOTTOM)),
+			TaintElement.CLEAN,
+			null);
+	private static final TimeSynchronizationAbstractDomain BOTTOM = new TimeSynchronizationAbstractDomain(
+			null,
+			TaintElement.BOTTOM,
+			null);
+
+	/**
+	 * Builds an initial symbolic stack.
+	 */
+	public TimeSynchronizationAbstractDomain() {
+		this(new ArrayList<>(Collections.nCopies(STACK_LIMIT, TaintElement.BOTTOM)), TaintElement.CLEAN, null);
+	}
+
+	/**
+	 * Builds a taint abstract stack.
+	 *
+	 * @param stack the stack of values
+	 */
+	protected TimeSynchronizationAbstractDomain(ArrayList<TaintElement> stack, TaintElement memory, EVMCFG cfg) {
+		super(stack, memory, cfg);
+	}
+
+	@Override
+	public boolean isTainted(Statement stmt) {
+		return stmt instanceof Timestamp
+				|| stmt instanceof Number
+				|| stmt instanceof Difficulty
+				|| stmt instanceof Gaslimit
+				|| stmt instanceof Gasprice
+				|| stmt instanceof Basefee
+				|| stmt instanceof Coinbase
+				|| stmt instanceof Blockhash;
+	}
+
+	@Override
+	public Set<Operator> getSanitizedOpcode() {
+		return Set.of();
+	}
+
+	@Override
+	public TimeSynchronizationAbstractDomain top() {
+		return TOP;
+	}
+
+	@Override
+	public TimeSynchronizationAbstractDomain bottom() {
+		return BOTTOM;
+	}
+
+	@Override
+	public TaintAbstractDomain mk(ArrayList<TaintElement> list, TaintElement memory, EVMCFG cfg) {
+		return new TimeSynchronizationAbstractDomain(list, memory, cfg);
+	}
+}
