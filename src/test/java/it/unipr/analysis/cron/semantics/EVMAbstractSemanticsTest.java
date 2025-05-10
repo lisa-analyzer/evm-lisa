79e43251db719ac3ae5c11832a6a66cf9c9d4a56
VincenzoArceri <vincenzoarceri.92@gmail.com>
2025-05-10 16:36:00 +0200

    Add test for MSTORE

--- src/test/java/it/unipr/analysis/cron/semantics/EVMAbstractSemanticsTest.java
+++ src/test/java/it/unipr/analysis/cron/semantics/EVMAbstractSemanticsTest.java
@@ -97,6 +97,12 @@ public class EVMAbstractSemanticsTest extends EVMBytecodeAnalysisExecutor {
 		CronConfiguration conf = createConfiguration("cfs", "mstore", "mstore_eth.sol", false);
 		perform(conf);
 	}
+	
+	@Test
+	public void testMstore2() throws AnalysisSetupException, IOException {
+		CronConfiguration conf = createConfiguration("cfs", "mstore2", "mstore_eth.sol", false);
+		perform(conf);
+	}
 
 	@Test
 	public void testMstore8() throws AnalysisSetupException, IOException {
