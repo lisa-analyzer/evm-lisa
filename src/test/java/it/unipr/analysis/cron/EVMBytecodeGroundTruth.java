1ba074f231cc1ac6300550f2e5cb0fefae525dd7
Luca Negrini <luca.negrinii@gmail.com>
2025-05-13 21:23:49 +0200

    Util method for regenerating ground truth baseline

--- src/test/java/it/unipr/analysis/cron/EVMBytecodeGroundTruth.java
+++ src/test/java/it/unipr/analysis/cron/EVMBytecodeGroundTruth.java
@@ -2,12 +2,18 @@ package it.unipr.analysis.cron;
 
 import it.unipr.EVMLiSA;
 import it.unipr.utils.JSONManager;
-import it.unipr.utils.StatisticsObject;
+import it.unipr.utils.StandardStatisticsObject;
+
+import static org.junit.Assert.assertFalse;
+
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Set;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /*
@@ -17,30 +23,41 @@ import org.junit.Test;
 public class EVMBytecodeGroundTruth {
 	private static final Logger log = LogManager.getLogger(EVMBytecodeGroundTruth.class);
 
-	@Test
-	public void testGroundTruth() throws Exception {
-		Path WORKING_DIRECTORY_PATH = Paths.get("evm-testcases", "ground-truth", "50-ground-truth");
-		Path GROUND_TRUTH_FILE_PATH = WORKING_DIRECTORY_PATH.resolve("ground-truth-data.json");
-		Path SMARTCONTRACTS_FULLPATH = Paths.get("benchmark", "50-ground-truth.txt");
+	private final Path WORKING_DIRECTORY_PATH = Paths.get("evm-testcases", "ground-truth", "50-ground-truth");
+	private final Path GROUND_TRUTH_FILE_PATH = WORKING_DIRECTORY_PATH.resolve("ground-truth-data.json");
+	private final Path SMARTCONTRACTS_FULLPATH = Paths.get("benchmark", "50-ground-truth.txt");
+	private final Path RUN_RESULTS = WORKING_DIRECTORY_PATH.resolve("set-of-contracts").resolve("results.json");
 
-		boolean changed = false;
+	private void setupAndRun() {
 		EVMLiSA.setWorkingDirectory(WORKING_DIRECTORY_PATH);
-		EVMLiSA.setCores(
-				Runtime.getRuntime().availableProcessors() / 4 * 3);
+		EVMLiSA.setCores(Runtime.getRuntime().availableProcessors() / 4 * 3);
 		EVMLiSA.setTestMode();
 		EVMLiSA.analyzeSetOfContracts(SMARTCONTRACTS_FULLPATH);
+	}
 
-		Set<StatisticsObject> groundTruthData = JSONManager.readStatsFromJSON(GROUND_TRUTH_FILE_PATH);
-		Set<StatisticsObject> newData = JSONManager.readStatsFromJSON(
-				WORKING_DIRECTORY_PATH.resolve("set-of-contracts").resolve("results.json"));
+	@Test
+	@Ignore
+	public void regenerateGroundTruth() throws Exception {
+		setupAndRun();
+
+		Files.copy(RUN_RESULTS, GROUND_TRUTH_FILE_PATH, StandardCopyOption.REPLACE_EXISTING);
+	}
+
+	@Test
+	public void testGroundTruth() throws Exception {
+		setupAndRun();
+
+		boolean changed = false;
+		Set<StandardStatisticsObject> groundTruthData = JSONManager.readStatsFromJSON(GROUND_TRUTH_FILE_PATH);
+		Set<StandardStatisticsObject> newData = JSONManager.readStatsFromJSON(RUN_RESULTS);
 
 		log.info("Ground truth size: {}", groundTruthData.size());
 		log.info("New data size: {}", newData.size());
 
 		assert groundTruthData.size() == newData.size();
 
-		for (StatisticsObject groundTruthStats : groundTruthData) {
-			for (StatisticsObject newStats : newData) {
+		for (StandardStatisticsObject groundTruthStats : groundTruthData) {
+			for (StandardStatisticsObject newStats : newData) {
 				if (groundTruthStats.getAddress().equals(newStats.getAddress())
 						&& !groundTruthStats.equals(newStats)) {
 
@@ -81,6 +98,6 @@ public class EVMBytecodeGroundTruth {
 			}
 		}
 
-		assert !changed;
+		assertFalse(changed);
 	}
 }
