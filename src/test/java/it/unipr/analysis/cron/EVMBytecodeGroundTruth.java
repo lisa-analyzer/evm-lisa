76d59b84633597044bdd63abbb0f59b02df297b0
merendamattia <merenda.s.mattia@gmail.com>
2025-10-09 11:58:49 +0200

    Removed unused import statement for JUnit Ignore

--- src/test/java/it/unipr/analysis/cron/EVMBytecodeGroundTruth.java
+++ src/test/java/it/unipr/analysis/cron/EVMBytecodeGroundTruth.java
@@ -14,7 +14,6 @@ import java.nio.file.StandardCopyOption;
 import java.util.Set;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /*
